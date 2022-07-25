e9500672f2a6b62a3e8ec0e9abca440b851b0a63
Prince <pappiah00@gmail.com>
2022-07-25 18:45:59 +0000

    .

--- src/axios.js
+++ src/axios.js
@@ -5,5 +5,5 @@ const isDev = process.env.NODE_ENV === "development";
 export const api = axios.create({
   baseURL: isDev
     ? "http://localhost:5000/api/v1"
-    : "https://moolre-test.web.app/",
+    : "https://moolre-test.herokuapp.com/api/v1",
 });
